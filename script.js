7c90b669c1e82a50891b9bc9432606bfffde4f4f
NSNet <frostzerokill@gmail.com>
2025-11-04 09:46:27 +0700

    Add some code to prevent repeated animations.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const toggler = document.getElementById("Toggle");
 const menu = document.getElementById("Menu");
+const items = document.querySelectorAll("li");
 
 toggler.addEventListener("click", () => {
+  toggler.style.pointerEvents = "none";
   menu.classList.toggle("active");
+
+  let endedCount = 0;
+
+  items.forEach((item) => {
+    item.addEventListener(
+      "transitionend",
+      () => {
+        setTimeout(() => {
+          endedCount++;
+
+          // when transition (animation) run thought all the item
+          if (endedCount === items.length) {
+            toggler.style.pointerEvents = "";
+          }
+        }, 500);
+      },
+      { once: true }
+    );
+  });
 });
